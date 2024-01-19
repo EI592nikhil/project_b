94f3b720ab31cdebe7fb75754b19934df8e3864d
jeevan <jeevan.sengar@dentsu.com>
2024-01-19 21:36:04 +0530

    Revert "Task_ID_118_Redirect_CUstomer_as_a_Trainer_to_it's_order_page_instead_of_account_page_dynamically"
    This reverts commit e06cc8c2fcd6b7cbd1d3b5e1881e331b46b4b704.

--- app/code/Dentsu/Trainerb/Observer/Frontend/Customer/Login.php
+++ app/code/Dentsu/Trainerb/Observer/Frontend/Customer/Login.php
@@ -8,60 +8,36 @@ use Magento\Framework\Event\Observer;
 use Magento\Customer\Model\Session as CustomerSession;
 use Magento\Framework\UrlInterface;
 use Magento\Framework\App\Response\RedirectInterface;
-use Magento\Framework\App\ResponseInterface; 
 
-class Login implements ObserverInterface
+class Login implements \Magento\Framework\Event\ObserverInterface
 {
     protected $customerSession;
     protected $urlInterface;
     protected $redirect;
-    protected $response; 
 
     public function __construct(
         CustomerSession $customerSession,
         UrlInterface $urlInterface,
-        RedirectInterface $redirect,
-        ResponseInterface $response
+        RedirectInterface $redirect
     ) {
         $this->customerSession = $customerSession;
         $this->urlInterface = $urlInterface;
         $this->redirect = $redirect;
-        $this->response = $response;
     }
 
     public function execute(Observer $observer)
     {
-        // Code to work on redirect with hard coded customer IDs
-         // $customer = $observer->getEvent()->getCustomer();  // code
-       //   $groupId = $customer->getGroupId(); // code
-       // if ($groupId == '1') {   // code
-            // Redirect to 'My Orders' section  (Comment)
-            //$this->redirect->redirect($this->customerSession->getBeforeAuthUrl() ?: $this->urlInterface->getUrl('sales/order/history')); (Comment)
+        $customer = $observer->getEvent()->getCustomer();
+        $groupId = $customer->getGroupId();
 
-      // $this->redirect->redirect($this->response, 'sales/order/history');  //code
-
-            //   $this->redirect->redirect($this->customerSession->getBeforeAuthUrl() ?: $this->urlInterface->getUrl('sales/order/history'));
-
-       // } // code
-
-
-
-
-
-        // Code to work on redirect to fetch customer group ID dynamically
-        $customerGroupId = $this->customerSession->getCustomerGroupId(); // code
         // Check if the customer group is 'Trainer'
-        if ($customerGroupId == ' ') {  // code
+        if ($groupId =='2') {
             // Redirect to 'My Orders' section
             //$this->redirect->redirect($this->customerSession->getBeforeAuthUrl() ?: $this->urlInterface->getUrl('sales/order/history'));
-
-            $this->redirect->redirect($this->response, 'sales/order/history');  // code
-
-            //   $this->redirect->redirect($this->customerSession->getBeforeAuthUrl() ?: $this->urlInterface->getUrl('sales/order/history'));
-
-         } // code
+        
+            $this->_redirect->redirect($this->_response, 'sales/order/history');
+        }
 
     }
 }
-
 
